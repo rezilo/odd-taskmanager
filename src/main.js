c0dfdf5fde4e638d43c73891b1dc7f36805af917
Ilya <ilya.shcoder@gmail.com>
2020-08-27 19:29:02 +0300

    Обновляю main.js
    Добавляю функцию с шаблоном меню таскменеджера
    Объявляю функцию render(), которая будет отрисовывать элементы интерфейса будующего приложения

---
+++ src/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const createSiteMenuTemplate = () => {
+  return (
+    `<section class="control__btn-wrap">
+      <input
+        type="radio"
+        name="control"
+        id="control__new-task"
+        class="control__input visually-hidden"
+      />
+      <label for="control__new-task" class="control__label control__label--new-task"
+        >+ ADD NEW TASK</label
+      >
+      <input
+        type="radio"
+        name="control"
+        id="control__task"
+        class="control__input visually-hidden"
+        checked
+      />
+      <label for="control__task" class="control__label">TASKS</label>
+      <input
+        type="radio"
+        name="control"
+        id="control__statistic"
+        class="control__input visually-hidden"
+      />
+      <label for="control__statistic" class="control__label"
+        >STATISTICS</label
+      >
+    </section>`
+  );
+};
+
+const render = (container, template, place) => {
+    container.insertAdjacentHTML(place, template);
+  };
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
