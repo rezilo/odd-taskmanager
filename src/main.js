5e3a2d4ad6b46852a051f1484662677dc5207c96
Ilya <ilya.shcoder@gmail.com>
2020-09-23 20:52:31 +0300

    Переписал main.js
    После пройденых новых материалов переписал модули по уму и весь файл main.js

--- src/main.js
+++ src/main.js
@@ -1,31 +1,55 @@
-/* eslint-disable no-plusplus */
-import createSiteMenuTemplate from './components/site-menu';
-import createFilterTemplate from './components/filter-menu';
-import createSortingModuleTemplate from './components/sorting';
-import createTaskEditTemplate from './components/task-edit';
-import createTaskTemplate from './components/task-template';
-import createLoadMoreButtonTemplate from './components/load-more';
+import { createSiteMenuTemplate } from './view/site-menu.js';
+import { createFilterTemplate } from './view/filter.js';
+import { createTaskTemplate } from './view/task.js';
+import { createTaskEditTemplate } from './view/task-edit.js';
+import { createLoadMoreButtonTemplate } from './view/load-more-button.js';
+import { createBoardTemplate } from './view/board.js';
+import { generateTask } from './mock/task.js';
+import { generateFilter } from './mock/filter.js';
 
-const render = (container, template, place = 'beforeend') => {
+const TASK_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
+
+const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
-render(siteMainElement, createSortingModuleTemplate());
+render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
+render(siteMainElement, createFilterTemplate(filters), `beforeend`);
+render(siteMainElement, createBoardTemplate(), `beforeend`);
 
-const boardElement = siteMainElement.querySelector('.board');
-const taskListElement = boardElement.querySelector('.board__tasks');
+const boardElement = siteMainElement.querySelector(`.board`);
+const taskListElement = boardElement.querySelector(`.board__tasks`);
 
-const TASK_COUNT = 3;
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 
-render(taskListElement, createTaskEditTemplate());
-
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
-render(boardElement, createLoadMoreButtonTemplate());
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
