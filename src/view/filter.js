7fad6e287ded390bac078a83c7b46e13ecba1687
Ilya <ilya.shcoder@gmail.com>
2020-09-23 20:38:54 +0300

    Добавляю модуль фильтра

---
+++ src/view/filter.js
@@ -0,0 +1,25 @@
+const createFilterItemTemplate = (filter, isChecked) => {
+  const { name, count } = filter;
+
+  return `<input
+      type="radio"
+      id="filter__${name}"
+      class="filter__input visually-hidden"
+      name="filter"
+      ${isChecked ? 'checked' : ''}
+      ${count === 0 ? 'disabled' : ''}
+    />
+    <label for="filter__${name}" class="filter__label">
+      ${name} <span class="filter__${name}-count">${count}</span></label
+    >`;
+};
+
+export const createFilterTemplate = (filterItems) => {
+  const filterItemsTemplate = filterItems
+    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
+    .join('');
+
+  return `<section class="main__filter filter container">
+    ${filterItemsTemplate}
+  </section>`;
+};
